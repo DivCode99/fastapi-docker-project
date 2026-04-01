0543ff0b9a0af520cf02c4ec8f16be14c10fb27e
Divyanshu Shekhar <divyanshushekhar@Divyanshus-MacBook-Pro.local>
2026-04-02 02:15:29 +0530

    Complete FastAPI Docker CI/CD project

--- dbconfig.py
+++ dbconfig.py
@@ -1,8 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_url = "postgresql://postgres:Anubhav*1234@localhost:5432/fastapidb"
+db_url = "postgresql://postgres:password@db:5432/fastapidb"
 
-engine = create_engine(db_url)
+import time
+from sqlalchemy import create_engine
+
+for i in range(10):
+    try:
+        engine = create_engine(db_url)
+        connection = engine.connect()
+        print("DB Connected ✅")
+        break
+    except Exception as e:
+        print("Waiting for DB... ⏳")
+        time.sleep(2)
 
 session = sessionmaker(autoflush=False, autocommit=False,bind=engine)
